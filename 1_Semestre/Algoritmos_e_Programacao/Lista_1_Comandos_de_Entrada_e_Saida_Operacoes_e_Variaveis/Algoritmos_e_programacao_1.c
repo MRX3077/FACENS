e9420a00325099a0e1002e0eec7aba007fd52b21
MRX2077 <igoremailtrabalho123@gmail.com>
2026-04-24 11:37:08 -0300

    Exercício 1: Receber um número, calcular seu dobro e exibir o resultado.

---
+++ 1_Semestre/Algoritmos_e_Programacao/Lista_1_Comandos_de_Entrada_e_Saida_Operacoes_e_Variaveis/Algoritmos_e_programacao_1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main(){
+	int a, mult;
+	printf("Digite um n\243mero para ser multiplicado por 2:");
+	scanf("%d",&a);
+	mult = a * 2;
+	printf("\n %d * 2 = %d",a,mult);
+}
